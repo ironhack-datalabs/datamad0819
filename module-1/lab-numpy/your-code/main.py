cc07ba9c25fa028d4e931a6d115e5e61a714bf89
rsd13 <rsoriadiez@gmail.com>
2019-08-23 15:44:13 +0200

    realizados todos menos el 15

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,120 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
+print("----------EJERCICIO 2------------\n")
+print(np.version.version)
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+print("----------EJERCICIO 3------------\n")
 
+a = np.random.random((2,3,5))
 
 
 #4. Print a.
-
+print("----------EJERCICIO 4------------\n")
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+print("----------EJERCICIO 5------------\n")
+b = np.ones((5,2,3))
 
 
 #6. Print b.
+print("----------EJERCICIO 6------------\n")
+
+print(b)
 
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+print("----------EJERCICIO 7------------\n")
 
-
-
+if a.size == b.size:
+    print("El tamaño son iguales")
+else:
+    print("El tamaño no son iguales")
 
 #8. Are you able to add a and b? Why or why not?
+print("----------EJERCICIO 8------------\n")
+
+try:
+    x = a + b
+    print("Se puede sumar porque las dimensiones de las dos matrices son iguales")
+except:
+    print("No se puede sumar porque las dimensiones de las dos matrices son distintas")
+
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+print("----------EJERCICIO 9------------\n")
+#dimensiones originales: 5x2x3
+#en transpose se intercambian las columnas como tu quieras
+c = b.transpose(1,2,0)
 
-
+print(c)
+#Con <shape> se ve las dimensiones
+print("Las dimension de la matriz c(transpuesta de b) es",c.shape)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+print("----------EJERCICIO 10------------\n")
+try:
+    d = a + c
+    print(d)
+    print("Se puede sumar porque las dimensiones de las dos matrices son iguales")
+except:
+    print("No se puede sumar porque las dimensiones de las dos matrices son distintas")
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print("----------EJERCICIO 11------------\n")
+print(a)
+print("-----------------")
+print(c)
 
-
-
+print("La diferencia es que que la matriz a es 2x3x5 y b es 3x2x5  ")
 
 #12. Multiply a and c. Assign the result to e.
+print("----------EJERCICIO 12------------\n")
+e = np.multiply(a, c)
 
+print(e)
+ 
 
 
 #13. Does e equal to a? Why or why not?
+#12. Multiply a and c. Assign the result to e.
+print("----------EJERCICIO 13------------\n")
 
+print("Las matrices e y a son iguales, porque  a multiplicar a y c. A ser c todos a 1 es iguales")
 
+e = a*c
+print(e)
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+print("----------EJERCICIO 14------------\n")
 
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
-
+print("El máximo de la matrix d es",d_max)
+print("El mínimo de la matrix d es",d_min)
+print("El valor medio de la matrix d es",d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+print("----------EJERCICIO 15------------\n")
+f = np.empty((2,3,5))
 
+print(f)
 
 
 
