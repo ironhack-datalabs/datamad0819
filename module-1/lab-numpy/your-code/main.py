b61fe5cb60494d677d1b284e31e252f780690b43
ferrero-felipe <ferrero.felipe@gmail.com>
2019-08-23 18:29:45 +0200

    lab completed

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,3 +1,7 @@
+
+### Lab Solved and Completed
+### Answers are made available to be read in terminal if code executed.
+
 #1. Import the NUMPY package under the name np.
 
 import numpy as np
@@ -11,17 +15,17 @@ np.show_config()
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-print('\n# GENERATING RANDOM MATRIX WITH:\n#    a = np.random.rand(2,3,5)')
+print('\n# GENERATING RANDOM MATRIX WITH:\n    a = np.random.rand(2,3,5)')
 a = np.random.rand(2,3,5)
 print(a)
 
-print('\n# GENERATING RANDOM MATRIX WITH:\n#    import random')
-print('#    a = np.array([[[random.random() for i in range(5)] for j in range(3)] for k in range(2)])')
+print('\n# GENERATING RANDOM MATRIX WITH:\n    import random')
+print('    a = np.array([[[random.random() for i in range(5)] for j in range(3)] for k in range(2)])')
 import random
 a = np.array([[[random.random() for i in range(5)] for j in range(3)] for k in range(2)])
 print(a)
 
-print('\n# GENERATING RANDOM MATRIX WITH:\n#    a = np.random.random((2,3,5))')
+print('\n# GENERATING RANDOM MATRIX WITH:\n    a = np.random.random((2,3,5))')
 a = np.random.random((2,3,5))
 
 #4. Print a.
@@ -71,7 +75,7 @@ print('\n# Matrices a and c may be added because both have the same shape.\n')
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-print('# Matrix a:\n', a,'\n\nMatrix d = a+c:\n',d)
+print('# Matrix a:\n', a,'\n\n# Matrix d = a+c:\n',d)
 print('\n The values of d are the values of a, with the same index, incremented by 1.\n')
 
 #12. Multiply a and c. Assign the result to e.
@@ -87,12 +91,17 @@ print(" They are equal because 'a*c' multiplies every element of a by 1.\n")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
+print('# Maximum value in matrix d:\n', d_max) 
+print('# Minimum value in matrix d:\n', d_min) 
+print('# Mean of all values in matrix d:\n', d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f=np.empty_like(d)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -102,8 +111,27 @@ Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
-
 """
+
+print("\n# Assigning values 0, 25, 50, 75 and 100 to matrix f from d, based on the distribution of it's values.")
+f = np.array([[[0 if num == d_min else 50 if num == d_mean else 100 if num == d_max else 25 if (num>d_min and num<d_mean) else 75 if (num>d_mean and num<d_max) else 'A' for num in d[i][j] ]for j in range(len(d[i]))]for i in range(len(d))], dtype='float64')
+
+'''
+# Alternatively:
+for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+                for k in range(d.shape[2]):
+                        if d[i][j][k] == d_min:
+                                f[i][j][k] = 0
+                        if d[i][j][k] == d_mean:
+                                f[i][j][k] = 50
+                        if d[i][j][k] == d_max:
+                                f[i][j][k] = 100
+                        if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                                f[i][j][k] = 25
+                        if d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                                f[i][j][k] = 75
+'''
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -126,6 +154,10 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print('\n# Matrix d:')
+print(d)
+print('\n# Matrix f:')
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -139,3 +171,17 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+# Assigning A to the highest value as in grades:
+
+e = np.empty_like(d,str)
+letter = {100.:'A',75.:'B',50.:'C',25.:'D',0.:'E'}
+e = np.vectorize(lambda x: letter[x],[str])(f)
+print('\n# Converting values of f to letters (A highest, E lowest):')
+print(e)
+
+'''
+Alternativetely, with an iteration of elements:
+for index in np.ndindex(f.shape):
+        e[index] = letter[f[index]]
+'''
