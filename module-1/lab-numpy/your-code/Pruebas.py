fce50134acdbdde2b5e64a6c134019bf5c528dc7
David <D.barrachina.aguerri@gmail.com>
2019-08-23 19:18:08 +0200

    Segunda prueba

---
+++ module-1/lab-numpy/your-code/Pruebas.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.random.rand(1,3,3)
+
+
+a.shape
+
+
+
--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,23 +1,40 @@
 #1. Import the NUMPY package under the name np.
 
-#Pull Request
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.version.version)
+
+print(np.show_config())
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.rand(2,3,5)
+
+b = np.random.randint(1,3,3)
+
+c = np.random.uniform(1,100,(1,2,3))
+
+
+
+
+
+
+
+
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
+
 
 
 
