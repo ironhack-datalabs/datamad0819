49c6a927377526a607199982d5c02d512d586d8b
Gonzalo Bandeira <gonzalobandeira@gmail.com>
2019-08-27 18:19:20 +0200

    modificaciones

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -26,7 +26,7 @@ SELECT
     au_lname AS 'LAST NAME',
     au_fname AS 'FIRST NAME',
     p.pub_name AS PUBLISHER,
-    count(t.title_id) as "TITLE COUNT"
+    COUNT(t.title_id) AS 'TITLE COUNT'
 FROM
     publications.authors AS au
         LEFT JOIN
@@ -37,7 +37,8 @@ FROM
     publishers AS p ON t.pub_id = p.pub_id
 WHERE
     p.pub_name IS NOT NULL
-group by t.title_id
+GROUP BY au.au_id , p.pub_id
+ORDER BY COUNT(t.title_id) DESC
 ;
 
 #Challenge 3
@@ -45,13 +46,14 @@ SELECT
     au.au_id AS 'AUTHOR ID',
     au.au_lname AS 'LAST NAME',
     au.au_fname AS 'FIRST NAME',
-    SUM(s.qty) AS TOTAL
+    sum(t.ytd_sales) as TOTAL
 FROM
     publications.authors AS au
         LEFT JOIN
     titleauthor AS t_au ON au.au_id = t_au.au_id
         LEFT JOIN
-    sales AS s ON t_au.title_id = s.title_id
+	titles as t on t_au.title_id = t.title_id
+    #sales AS s ON t_au.title_id = s.title_id
 WHERE
     t_au.au_id IS NOT NULL
 GROUP BY au.au_id
@@ -64,14 +66,14 @@ SELECT
     au.au_id AS 'AUTHOR ID',
     au.au_lname AS 'LAST NAME',
     au.au_fname AS 'FIRST NAME',
-	ifnull(SUM(s.qty),0) AS TOTAL
+	ifnull(sum(t.ytd_sales),0) as TOTAL
 FROM
     publications.authors AS au
         LEFT JOIN
     titleauthor AS t_au ON au.au_id = t_au.au_id
         LEFT JOIN
-	sales AS s ON t_au.title_id = s.title_id
-    #titles AS t ON t_au.title_id = t.title_id
+	#sales AS s ON t_au.title_id = s.title_id
+    titles AS t ON t_au.title_id = t.title_id
 GROUP BY au.au_id
 ORDER BY TOTAL DESC
 ;
@@ -82,7 +84,7 @@ SELECT
     au.au_id AS AUTHOR,
     au.au_lname AS 'LAST NAME',
     au.au_fname AS 'FIRST NAME',
-    SUM(t.ytd_sales * t.price * t.royalty * ta.royaltyper / 100 + t.advance) AS PROFIT
+    round(SUM(t.ytd_sales * t.price * t.royalty/100 * ta.royaltyper / 100 + t.advance*ta.royaltyper/100),2) AS PROFIT
 FROM
     authors AS au
         LEFT JOIN
@@ -90,6 +92,6 @@ FROM
         LEFT JOIN
     titles AS t ON ta.title_id = t.title_id
 GROUP BY au.au_id
-ORDER BY advance DESC
+ORDER BY profit DESC
 LIMIT 3
 ;
