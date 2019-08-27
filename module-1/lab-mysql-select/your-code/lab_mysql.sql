5371eb0644afb46b8933cc0a97cf9c7981c30f4a
DavidR81 <davidrecuero@gmail.com>
2019-08-27 22:06:03 +0200

    poco a poco

--- module-1/lab-mysql-select/your-code/lab_mysql.sql
+++ module-1/lab-mysql-select/your-code/lab_mysql.sql
@@ -1,11 +1,11 @@
 #challengue 1
 
-SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER'
 FROM lab_mysql.authors
-LEFT JOIN lab_mysql.titleauthor
+LEFT JOIN titleauthor
 ON authors.au_id = titleauthor.au_id
-LEFT JOIN lab_mysql.titles
+LEFT JOIN titles
 ON titleauthor.title_id = titles.title_id
-LEFT JOIN lab_mysql.publishers
+LEFT JOIN publishers
 ON titles.pub_id = publishers.pub_id
 WHERE title IS NOT NULL;
