d333c68d66de9c2f5d2cec545360f75605a62082
Dededavid <davidperez2121@gmail.com>
2019-08-27 21:50:51 +0200

    añadido soluciones PARTE 1

---
+++ module-1/lab-mysql-select/Soluciones_ejercicio.sql
@@ -0,0 +1,19 @@
+#CHALLENGE_1
+
+SELECT 
+authors.au_id AS "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME", 
+titles.title AS "TITLE", 
+publishers.pub_name AS "PUBLISHER"
+FROM publications.authors
+
+LEFT JOIN publications.titleauthor
+ON authors.au_id = titleauthor.au_id
+
+LEFT JOIN publications.titles
+ON titleauthor.title_id = titles.title_id
+
+LEFT JOIN publications.publishers
+ON titles.pub_id = publishers.pub_id
+WHERE title IS NOT Null
