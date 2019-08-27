74be8156b31f4b431a189cf9596b279a9c4bc5c5
DavidR81 <davidrecuero@gmail.com>
2019-08-27 23:01:05 +0200

    falta bastante

--- module-1/lab-mysql-select/your-code/lab_mysql.sql
+++ module-1/lab-mysql-select/your-code/lab_mysql.sql
@@ -1,7 +1,8 @@
 #challengue 1
 
-SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER'
-FROM lab_mysql.authors
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', 
+titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER'
+FROM authors
 LEFT JOIN titleauthor
 ON authors.au_id = titleauthor.au_id
 LEFT JOIN titles
@@ -9,3 +10,20 @@ ON titleauthor.title_id = titles.title_id
 LEFT JOIN publishers
 ON titles.pub_id = publishers.pub_id
 WHERE title IS NOT NULL;
+
+#chellengue 2
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', 
+authors.au_fname as 'FIRST NAME', titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER',
+ COUNT(t.title_id) as 'TITLE COUNT'
+
+FROM authors
+LEFT JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles
+ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers
+ON titles.pub_id = publishers.pub_id
+WHERE t.title != "null"
+GROUP BY au.au_id,publishers.pub_id, titles.title_id
+ORDER BY COUNT(t.title_id) DESC
