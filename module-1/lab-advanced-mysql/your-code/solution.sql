03570ff4e0dfae2ae8f5d5db86f54ba99328e12c
Curro <farc@outlook.com>
2019-08-28 23:03:20 +0200

    Finalizado

--- module-1/lab-advanced-mysql/your-code/solution.sql
+++ module-1/lab-advanced-mysql/your-code/solution.sql
@@ -63,3 +63,31 @@ ORDER BY PROFIT DESC
 LIMIT 3;
 
 drop temporary table titleAuthorProfits;
+
+#CHALLENGE 3
+
+CREATE temporary table titleAuthorProfits(
+	SELECT titau.title_id as title_id,titau.au_id as au_id,(t.price*(t.royalty)*(titau.royaltyper/100)) as ROYALTY_U ,(t.advance*(titau.royaltyper/100)) AS ADVANCE_TITLE
+	FROM titleauthor as titau
+	JOIN titles as t
+	ON titau.title_id = t.title_id
+	ORDER BY title_id,au_id);
+
+CREATE TABLE most_profiting_authors(
+SELECT ta.au_id, SUM(((sales.qty)*ta.ROYALTY_U)+ ta.ADVANCE_TITLE) AS PROFIT
+FROM titleAuthorProfits as ta
+JOIN sales
+ON ta.title_id = sales.title_id
+group by ta.au_id
+ORDER BY PROFIT DESC
+LIMIT 3
+);
+
+drop temporary table titleAuthorProfits;
+
+SELECT * FROM most_profiting_authors;
+
+#para volver a ejecutar el create_table hacer el drop de abajo
+#DROP TABLE most_profiting_authors;
+
+
