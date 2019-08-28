37b036bb60ab32f872d0da9e7903bfb06529d504
Ines P. Chavarri <inespchavarri@gmail.com>
2019-08-28 21:34:53 +0200

    reinicio ejercicio

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -5,8 +5,8 @@
 
 CREATE TEMPORARY TABLE Royalties
 SELECT 
-    titles.title_id, #AS 'TITLE_ID',
-    titleauthor.au_id, #AS 'AUTHOR_ID',
+    titles.title_id AS 'TITLE_ID',
+    titleauthor.au_id AS 'AUTHOR_ID',
     SUM((titles.price * sales.qty * titles.royalty) / 100 * titleauthor.royaltyper / 100)  AS 'ROYALTY'
 FROM
     publications.titles
@@ -20,8 +20,11 @@ ORDER BY 2;
 # STEP 2
 # TITLE ID, AUTHOR ID, AGGREGATED ROYALTIES (use the SUM subquery and group by both au_id and title_id)
 
+#DROP TEMPORARY TABLE royalties;
 
-SELECT TITLE_ID, AUTHOR_ID, SUM(royalty)
+SELECT title_id, author_id, SUM(royalty)
 FROM Royalties
 GROUP BY 1, 2
 ORDER BY 3 DESC;
+
+# STEP 3
