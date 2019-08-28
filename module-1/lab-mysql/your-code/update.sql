c372a0f6780d7046e7d852d623e1e14d64436c74
hermenegildo martinez-alcala <hermenegildomartinez-alcala@MacBook-Pro-de-hermenegildo.local>
2019-08-28 20:22:44 +0200

    Final

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,5 @@
+UPDATE `lab-mysql`.`salespersons` SET `store` = 'Miami' WHERE (`idsalespersons` = '4');
+
+UPDATE `lab-mysql`.`customers` SET `email` = 'ppicasso@gmail.com' WHERE (`idcustomers` = '0');
+UPDATE `lab-mysql`.`customers` SET `email` = 'lincoln@us.gov' WHERE (`idcustomers` = '1');
+UPDATE `lab-mysql`.`customers` SET `email` = 'hello@napoleon.me' WHERE (`idcustomers` = '2');
