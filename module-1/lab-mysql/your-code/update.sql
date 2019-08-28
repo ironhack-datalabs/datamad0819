583b23c9415023e0cab16d9736ee01660c1efa11
Your Name <jsl.carranza@gmail.com>
2019-08-28 20:27:55 +0200

    Terminado con bonus

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,8 @@
+USE cars_shop_1;
+
+UPDATE `salespersons` SET `Store` = 'Miami' WHERE `Store` = 'Mimia' AND ID = 5;
+
+
+UPDATE `customers` SET `Email` ='ppicasso@gmail.com' WHERE `Name` = 'Pablo Picasso' AND ID = 1;
+UPDATE `customers` SET `Email` ='lincoln@us.gov' WHERE `Name` = 'Abraham Lincoln' AND ID = 2;
+UPDATE `customers` SET `Email` ='hello@napoleon.me' WHERE `Name` = 'Napoléon Bonaparte' AND ID = 3; 
