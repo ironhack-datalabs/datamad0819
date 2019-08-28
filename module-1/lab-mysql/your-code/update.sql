6bd677b31b4810ffb4afb09e6cbdedc8d4813a8b
Ines P. Chavarri <inespchavarri@gmail.com>
2019-08-28 16:55:35 +0200

    Ejercicio update

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE salespersons
+SET store = 'Miami'
+WHERE idStaff = 5;
+
+UPDATE customers
+SET email = 'ppicasso@gmail.com'
+WHERE idCustomers = 1;
+
+UPDATE customers
+SET email = 'lincoln@us.gov'
+WHERE idCustomers = 2;
+
+UPDATE customers
+SET email = 'hello@napoleon.me'
+WHERE idCustomers = 3;
