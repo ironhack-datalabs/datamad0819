b14885ab064e415222bbf2c492ba58ee86bd35dd
Ines P. Chavarri <inespchavarri@gmail.com>
2019-08-28 19:16:06 +0200

    Inicio ejercicio

---
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -0,0 +1,27 @@
+# CHALLANGE 1 - STEP 1
+# TITLE ID, AUTHOR ID, ROYALTY
+# ROYALTY sales_royalty = titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100
+# TITLES -- TITLEAUTHOR -- SALES 
+
+CREATE TEMPORARY TABLE Royalties
+SELECT 
+    titles.title_id, #AS 'TITLE_ID',
+    titleauthor.au_id, #AS 'AUTHOR_ID',
+    SUM((titles.price * sales.qty * titles.royalty) / 100 * titleauthor.royaltyper / 100)  AS 'ROYALTY'
+FROM
+    publications.titles
+        INNER JOIN
+    publications.titleauthor ON titles.title_id = titleauthor.title_id
+        INNER JOIN
+    publications.sales ON titleauthor.title_id = sales.title_id
+GROUP BY 2, 1
+ORDER BY 2;
+
+# STEP 2
+# TITLE ID, AUTHOR ID, AGGREGATED ROYALTIES (use the SUM subquery and group by both au_id and title_id)
+
+
+SELECT TITLE_ID, AUTHOR_ID, SUM(royalty)
+FROM Royalties
+GROUP BY 1, 2
+ORDER BY 3 DESC;
