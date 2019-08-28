37e001df7be242b98f48d6a0b6270c5eeaaab7e6
Ines P. Chavarri <inespchavarri@gmail.com>
2019-08-28 22:17:00 +0200

    Resolución ejercicio

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -10,21 +10,33 @@ SELECT
     SUM((titles.price * sales.qty * titles.royalty) / 100 * titleauthor.royaltyper / 100)  AS 'ROYALTY'
 FROM
     publications.titles
-        INNER JOIN
+        LEFT JOIN
     publications.titleauthor ON titles.title_id = titleauthor.title_id
-        INNER JOIN
+        LEFT JOIN
     publications.sales ON titleauthor.title_id = sales.title_id
 GROUP BY 2, 1
 ORDER BY 2;
 
+# DROP TEMPORARY TABLE royalties;
+
 # STEP 2
 # TITLE ID, AUTHOR ID, AGGREGATED ROYALTIES (use the SUM subquery and group by both au_id and title_id)
+# Sé que este segundo paso está mal, obtengo el mismo output que en el primero, pero no sé por qué
 
-#DROP TEMPORARY TABLE royalties;
 
+CREATE TEMPORARY TABLE Total
 SELECT title_id, author_id, SUM(royalty)
 FROM Royalties
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
 # STEP 3
+# Author ID, Profits of each author aggregating the advance and total royalties of each title
+
+SELECT author_id, ROUND(SUM(titles.advance + royalty), 2)
+FROM Total
+LEFT JOIN titles
+ON total.title_id = titles.title_id
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 3;
