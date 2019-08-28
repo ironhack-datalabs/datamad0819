7123c90c45e9e120905425dc89d4d8e0f573d30a
DavidR81 <davidrecuero@gmail.com>
2019-08-28 14:12:39 +0200

    falta el bonus

--- module-1/lab-mysql-select/your-code/lab_mysql.sql
+++ module-1/lab-mysql-select/your-code/lab_mysql.sql
@@ -1,26 +1,65 @@
-#challengue 1
+#challenge 1
 
-SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', 
-titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER'
-FROM authors
-LEFT JOIN titleauthor
-ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles
-ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers
-ON titles.pub_id = publishers.pub_id
-WHERE title IS NOT NULL;
+SELECT 
+    authors.au_id AS 'AUTHOR ID',
+    authors.au_lname AS 'LAST NAME',
+    authors.au_fname AS 'FIRST NAME',
+    titles.title AS 'TITLE',
+    publishers.pub_name AS 'PUBLISHER'
+FROM
+    authors
+        INNER JOIN
+    titleauthor ON authors.au_id = titleauthor.au_id
+        INNER JOIN
+    titles ON titleauthor.title_id = titles.title_id
+        INNER JOIN
+    publishers ON titles.pub_id = publishers.pub_id
 
-#chellengue 2
 
-SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', 
-titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER', COUNT(titles.title_id) as 'TITLE COUNT'
-FROM authors
-LEFT JOIN titleauthor
-ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles
-ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers
-ON titles.pub_id = publishers.pub_id
-WHERE title IS NOT NULL
-GROUP BY titles.title_id
+#chellenge 2
+
+SELECT 
+    authors.au_id AS 'AUTHOR ID',
+    authors.au_lname AS 'LAST NAME',
+    authors.au_fname AS 'FIRST NAME',
+    publishers.pub_name AS 'PUBLISHER',
+    COUNT(titles.title) AS 'TITLE COUNT'
+FROM
+    authors
+        INNER JOIN
+    titleauthor ON authors.au_id = titleauthor.au_id
+        INNER JOIN
+    titles ON titleauthor.title_id = titles.title_id
+        INNER JOIN
+    publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id , publishers.pub_id
+ORDER BY COUNT(titles.title_id) DESC
+;
+
+#chellenge 3
+
+SELECT 
+    authors.au_id AS 'AUTHOR ID',
+    authors.au_lname AS 'LAST NAME',
+    authors.au_fname AS 'FIRST NAME',
+    SUM(titles.ytd_sales) AS 'TOTAL'
+FROM
+    authors
+        INNER JOIN
+    titleauthor ON authors.au_id = titleauthor.au_id
+        INNER JOIN
+    titles ON titleauthor.title_id = titles.title_id
+        INNER JOIN
+    publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 3
+
+#cha
+
+
+
+
+
+
+
