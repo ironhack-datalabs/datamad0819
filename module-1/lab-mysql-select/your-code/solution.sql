6123d0092fe581798ce7936c6dcd0f8e747b12aa
pedromerry <pedro@merrydelval.eu>
2019-08-28 15:27:35 +0200

    solucion corregida tras feedback de Alberto

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -1,65 +1,74 @@
+#Challenge 1
 SELECT 
-	ta.au_id,au.au_lname,au.au_fname,t.title,p.pub_name
-FROM
-    titleauthor AS ta
-    LEFT JOIN authors AS au
-		ON ta.au_id=au.au_id
-	LEFT JOIN titles AS t
-		ON ta.title_id = t.title_id
-	LEFT JOIN publishers AS p
-		ON t.pub_id=p.pub_id;
-	
-    
-    SELECT 
-	ta.au_id,au.au_lname,au.au_fname,t.title,p.pub_name,count(ta.au_id) AS Numcount
-FROM
-    titleauthor AS ta
-    LEFT JOIN authors AS au
-		ON ta.au_id=au.au_id
-	LEFT JOIN titles AS t
-		ON ta.title_id = t.title_id
-	LEFT JOIN publishers AS p
-		ON t.pub_id=p.pub_id
-GROUP BY p.pub_id,au_id;
+ta.au_id as "AUTHOR ID",
+au.au_lname as "LAST NAME",
+au.au_fname as "FIRST NAME",
+t.title as "TITLE",
+p.pub_name as "PUBLISHER"
+FROM titleauthor AS ta LEFT JOIN authors AS au ON ta.au_id=au.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN publishers AS p ON t.pub_id=p.pub_id;
 
-SELECT
-	ta.au_id,au.au_lname,au.au_fname,sum(t.ytd_sales) as TotalSales 
+#Challenge 2
+SELECT 
+ta.au_id as "AUTHOR ID",
+au.au_lname as "LAST NAME",
+au.au_fname as "FIRST NAME",
+p.pub_name as "PUBLISHER",
+count(t.title) as "TITLE COUNT"
 FROM
-    titleauthor AS ta
-    LEFT JOIN authors AS au
-		ON ta.au_id=au.au_id
-	LEFT JOIN titles AS t
-		ON ta.title_id = t.title_id
-	LEFT JOIN publishers AS p
-		ON t.pub_id=p.pub_id
-GROUP BY ta.au_id
-ORDER BY TotalSales DESC
-limit 3;
+titleauthor AS ta
+LEFT JOIN authors AS au ON ta.au_id=au.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
+GROUP BY au.au_id;
 
-SELECT
-	au.au_id,au.au_fname,au.au_lname, sum(IFNULL(t.ytd_sales,0)) AS Total_Sales
-FROM authors as au
-    LEFT JOIN titleauthor AS ta
-		ON au.au_id=ta.au_id
-	LEFT JOIN titles AS t
-		ON ta.title_id=t.title_id
+#Challenge 3
+SELECT 
+ta.au_id as "AUTHOR ID",
+au.au_lname as "LAST NAME",
+au.au_fname as "FIRST NAME",
+count(t.title) as "TOTAL"
+FROM
+titleauthor AS ta
+LEFT JOIN authors AS au ON ta.au_id=au.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
 GROUP BY au.au_id
-ORDER BY Total_Sales DESC;
+ORDER BY count(t.title) DESC,`LAST NAME`
+LIMIT 3;
 
-SELECT
-	au.au_id,au.au_fname,au.au_lname, t.title,t.advance, ta.royaltyper,IFNULL(t.ytd_sales,0) AS Total_Sales, sum(t.advance+(ta.royaltyper/100)*IFNULL(t.ytd_sales,0)) AS Total_profit
-FROM authors as au
-    LEFT JOIN titleauthor AS ta
-		ON au.au_id=ta.au_id
-	LEFT JOIN titles AS t
-		ON ta.title_id=t.title_id
+#Challenge 4
+SELECT 
+ta.au_id as "AUTHOR ID",
+au.au_lname as "LAST NAME",
+au.au_fname as "FIRST NAME",
+IFNULL( count(t.title),0 ) as `TOTAL A`
+FROM
+titleauthor AS ta
+RIGHT JOIN authors AS au ON ta.au_id=au.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
 GROUP BY au.au_id
-ORDER BY Total_profit DESC	
+ORDER BY count(t.title) DESC,`LAST NAME`;
+
+#Bonus excercise
+SELECT 
+ta.au_id as "AUTHOR ID",
+au.au_lname as "LAST NAME",
+au.au_fname as "FIRST NAME",
+(ta.royaltyper/100)*(t.advance+IFNULL(t.ytd_sales,0)) as `PROFIT`
+FROM
+titleauthor AS ta
+RIGHT JOIN authors AS au ON ta.au_id=au.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN publishers AS p ON t.pub_id=p.pub_id
+GROUP BY `AUTHOR ID`
+ORDER BY `PROFIT` DESC
 LIMIT 3;
 
 
 
-	
 
-	
+
 	
