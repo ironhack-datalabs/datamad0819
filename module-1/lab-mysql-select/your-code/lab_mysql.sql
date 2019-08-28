a22dcea8187f61fdbd0a3d911781e4e65c85583f
DavidR81 <davidrecuero@gmail.com>
2019-08-28 09:29:56 +0200

    Falta challengues

--- module-1/lab-mysql-select/your-code/lab_mysql.sql
+++ module-1/lab-mysql-select/your-code/lab_mysql.sql
@@ -13,10 +13,8 @@ WHERE title IS NOT NULL;
 
 #chellengue 2
 
-SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', 
-authors.au_fname as 'FIRST NAME', titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER',
- COUNT(t.title_id) as 'TITLE COUNT'
-
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', 
+titles.title as 'TITLE', publishers.pub_name as 'PUBLISHER', COUNT(titles.title_id) as 'TITLE COUNT'
 FROM authors
 LEFT JOIN titleauthor
 ON authors.au_id = titleauthor.au_id
@@ -24,6 +22,5 @@ LEFT JOIN titles
 ON titleauthor.title_id = titles.title_id
 LEFT JOIN publishers
 ON titles.pub_id = publishers.pub_id
-WHERE t.title != "null"
-GROUP BY au.au_id,publishers.pub_id, titles.title_id
-ORDER BY COUNT(t.title_id) DESC
+WHERE title IS NOT NULL
+GROUP BY titles.title_id
