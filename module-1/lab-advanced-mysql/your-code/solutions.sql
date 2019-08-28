e9037c768cdf7ba8e7393f033f1396e264a99c0f
ferrero-felipe <ferrero.felipe@gmail.com>
2019-08-28 22:02:50 +0200

    retry solution

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,5 +1,120 @@
 USE publications;
 
+#SUB QUERIES
+SELECT
+    total_royalties.au_id,
+    sum(advance)+SUM(total_royalties) as profits,
+    au_fname,
+    au_lname
+FROM
+    (SELECT
+        title_id, 
+        au_id,
+        advance, 
+        sum(royalties) AS total_royalties
+    FROM
+        (SELECT
+            titles.title_id,
+            price,
+            advance,
+            royalty,
+            qty,
+            au_id,
+            royaltyper,
+            (qty*price*royalty*royaltyper/10000) AS royalties
+        FROM
+            titles
+            LEFT JOIN
+                sales
+                ON titles.title_id = sales.title_id
+            LEFT JOIN
+                titleauthor
+                ON titles.title_id = titleauthor.title_id) AS royalties
+    GROUP BY title_id, au_id) AS total_royalties
+    LEFT JOIN
+		authors
+        ON total_royalties.au_id = authors.au_id
+GROUP BY au_id
+ORDER BY profits DESC
+LIMIT 3;
+
+#TEMPORARY TABLES
+CREATE TEMPORARY TABLE royalties
+SELECT
+    titles.title_id,
+    price,
+    advance,
+    royalty,
+    qty,
+    au_id,
+    royaltyper,
+    (qty*price*royalty*royaltyper/10000) AS royalties
+FROM
+    titles
+    LEFT JOIN
+        sales
+        ON titles.title_id = sales.title_id
+    LEFT JOIN
+        titleauthor
+        ON titles.title_id = titleauthor.title_id;
+CREATE TEMPORARY TABLE total_royalties
+SELECT
+    title_id, 
+    au_id,
+    MAX(advance) AS advance, 
+    SUM(royalties) AS total_royalties
+FROM
+   royalties
+GROUP BY title_id, au_id;
+CREATE TEMPORARY TABLE profits
+SELECT
+    au_id,
+    SUM(advance)+SUM(total_royalties) AS profits
+FROM
+    total_royalties
+GROUP BY au_id;
+SELECT
+    profits.au_id,
+    profits,
+    au_fname,
+    au_lname
+FROM
+    profits
+    LEFT JOIN
+		authors
+        ON profits.au_id = authors.au_id
+ORDER BY profits DESC
+LIMIT 3;
+
+#CREATE PERMANENT TABLE
+CREATE TABLE most_profiting_authors
+SELECT
+    profits.au_id,
+    profits,
+    au_fname,
+    au_lname
+FROM
+    profits
+    LEFT JOIN
+		authors
+        ON profits.au_id = authors.au_id
+ORDER BY profits DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+'''''
+
+#FIRST TRY
+
 # SUB QUERIES
 SELECT
     profit.au_id,
@@ -67,10 +182,4 @@ FROM
     profit
     ORDER BY profit DESC
 LIMIT 3;
-
-# CREATE PERMANENT TABLE
-CREATE TABLE most_profiting_authors
-SELECT
-    profit.au_id,
-    (profit.total_royalties_all_books + profit.advance_all_books) as profit 
-FROM profit
+ '''''
