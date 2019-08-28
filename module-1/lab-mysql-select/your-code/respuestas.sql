408f567c5080261dd5f9ea4f78681e880ca8264d
hermenegildo martinez-alcala <hermenegildomartinez-alcala@MacBook-Pro-de-hermenegildo.local>
2019-08-28 09:54:08 +0200

    Por fin entiendo algo

--- module-1/lab-mysql-select/your-code/respuestas.sql
+++ module-1/lab-mysql-select/your-code/respuestas.sql
@@ -1,10 +1,12 @@
 #USE publications;
 SELECT authors.au_id as "AUTHOR ID", authors.au_lname as "LAST NAME",
-authors.au_fname as "FIRST NAME", titles.title_id as "TITLE"
+authors.au_fname as "FIRST NAME", titles.title_id as "TITLE", publishers.pub_name as "PUBLISHER"
 FROM authors
 INNER JOIN titleauthor ON authors.au_id=titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id=titles.title_id 
+INNER JOIN publishers ON titles.pub_id =publishers.pub_id
+ORDER BY authors.au_id
 
 
-#NO CONSIGO ENTENDER EL CONCEPTO
+#Estuve intentando entender el concepto lo que me demoro mucho.
 #Espero poder resolverlo mas adelante, porque ahora no consigo pillarlo.
