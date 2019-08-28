b414f5a4abb746d2ca94a39eedcb4bf8d6478966
ferrero-felipe <ferrero.felipe@gmail.com>
2019-08-28 19:09:51 +0200

    solution for challenge1

---
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -0,0 +1,149 @@
+USE publications;
+
+#FIRST ATTEMPT
+'''
+SELECT
+    total_royalties.au_id,
+    total_royalties.total_royalties+total_royalties.advance AS profit
+FROM
+    (SELECT 
+        royalties.title_id,
+        royalties.au_id,
+        SUM(royalties.sales_royalty) as total_royalties,
+        SUM(royalties.advance
+        FROM
+            (SELECT 
+                sales.title_id,
+                t.au_id, 
+                (t.price*sales.qty*(t.royalty/100)*(t.royaltyper/100)) AS sales_royalty,
+                t.advance
+            FROM
+                sales 
+                LEFT JOIN 
+                    (SELECT 
+                        titleauthor.au_id, 
+                        titleauthor.royaltyper, 
+                        titles.price, 
+                        titles.royalty,
+                        titles.title_id,
+                        titles.advance
+                    FROM
+                        titleauthor
+                        LEFT JOIN
+                            titles
+                        ON titleauthor.title_id = titles.title_id) AS t
+                ON
+                    sales.title_id = t.title_id) AS royalties
+        GROUP BY royalties.title_id, royalties.au_id) AS total_royalties
+ORDER BY profit DESC
+LIMIT 3
+;
+'''
+
+
+# SECOND ATTEMPT
+SELECT
+    profit.au_id,
+    (profit.total_royalties_all_books + profit.advance_all_books) as profit
+FROM
+    (SELECT
+        total_royalties.au_id,
+        SUM(total_royalties.total_royalties_each_book) AS total_royalties_all_books,
+        SUM(total_royalties.advance_each_book) AS advance_all_books
+    FROM
+        (SELECT
+            royalties.title_id,
+            MAX(royalties.au_id) as au_id,
+            SUM(royalties.sales_royalty) as total_royalties_each_book,
+            MAX(royalties.advance) as advance_each_book
+        FROM
+            (SELECT 
+                titles.title_id,
+                titleauthor.au_id, 
+                (titles.price*sales.qty*(titles.royalty/100)*(titleauthor.royaltyper/100)) AS sales_royalty,
+                titles.advance
+            FROM titleauthor
+                LEFT JOIN titles
+                ON titleauthor.title_id = titles.title_id
+                LEFT JOIN sales
+                ON titles.title_id = sales.title_id) as royalties
+            GROUP BY royalties.title_id) as total_royalties
+        GROUP BY total_royalties.au_id) as profit
+    ORDER BY profit DESC
+LIMIT 3;
+
+#THIRD ATTEMPT
+'''SELECT
+    profit.au_id,
+    (profit.total_royalties_all_books + profit.advance_all_books) as profit
+FROM
+    (SELECT
+        total_royalties.au_id,
+        SUM(total_royalties.total_royalties_each_book) AS total_royalties_all_books,
+        SUM(total_royalties.advance_each_book) AS advance_all_books
+    FROM
+        (SELECT
+            royalties.title_id,
+            MAX(royalties.au_id) as au_id,
+            SUM(royalties.sales_royalty) as total_royalties_each_book,
+            MAX(royalties.advance) as advance_each_book
+        FROM
+            (SELECT
+                t.title_id,
+                t.au_id, 
+                (t.price*sales.qty*(t.royalty/100)*(t.royaltyper/100)) AS sales_royalty,
+                t.advance
+            FROM
+                (SELECT 
+                    titles.title_id,
+                    titleauthor.au_id, 
+                    titles.advance,
+                    titles.price,
+                    titles.royalty,
+                    titleauthor.royaltyper
+                FROM titleauthor
+                    LEFT JOIN titles
+                ON titleauthor.title_id = titles.title_id) AS t
+                LEFT JOIN sales
+                ON t.title_id = sales.title_id) as royalties
+            GROUP BY royalties.title_id) as total_royalties
+        GROUP BY total_royalties.au_id) as profit
+    ORDER BY profit DESC
+LIMIT 3;'''
+
+# TEMPORARY TABLES
+CREATE TEMPORARY TABLE royalties
+    SELECT 
+        titles.title_id,
+        titleauthor.au_id, 
+        (titles.price*sales.qty*(titles.royalty/100)*(titleauthor.royaltyper/100)) AS sales_royalty,
+        titles.advance
+    FROM titleauthor
+        LEFT JOIN titles
+        ON titleauthor.title_id = titles.title_id
+        LEFT JOIN sales
+        ON titles.title_id = sales.title_id;
+CREATE TEMPORARY TABLE total_royalties
+    SELECT
+        royalties.title_id,
+        MAX(royalties.au_id) as au_id,
+        SUM(royalties.sales_royalty) as total_royalties_each_book,
+        MAX(royalties.advance) as advance_each_book
+    FROM
+        royalties
+        GROUP BY royalties.title_id;
+CREATE TEMPORARY TABLE profit
+    SELECT
+        total_royalties.au_id,
+        SUM(total_royalties.total_royalties_each_book) AS total_royalties_all_books,
+        SUM(total_royalties.advance_each_book) AS advance_all_books
+        FROM
+            total_royalties
+        GROUP BY total_royalties.au_id;
+SELECT
+    profit.au_id,
+    (profit.total_royalties_all_books + profit.advance_all_books) as profit
+FROM
+    profit
+    ORDER BY profit DESC
+LIMIT 3;
