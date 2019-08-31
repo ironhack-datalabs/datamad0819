e4f14e75d009aeb73667b19a3c5bc9a20bd6bebf
Curro <farc@outlook.com>
2019-08-31 18:56:25 +0200

    añadidos sexo y paises

--- module-1/pandas-project/your-code/functions.py
+++ module-1/pandas-project/your-code/functions.py
@@ -1,3 +1,5 @@
+import requests
+import threading
 def cleanTextInFatal(text):
     """
     devuelve "N" si text contiene N o N o f o F o # o devuelve "UNKNOWN" si contiene "unkn"
@@ -9,4 +11,70 @@ def cleanTextInFatal(text):
     else:
         return "Y"
 
+
+
+def cleanActivity(text,lista):
+    """Limpia la columna activity categorizando """
+    texto = text.upper()
+    if text not in lista:
+        return text
+    else:
+        texto = text.upper()
+        if texto.find("DIVING")!=-1:
+            return retornoScubaDiving(texto)
+        if texto.find("SURF")!=-1:
+            return retornoTiposSurf(texto)
+        if texto.find("FISH")!=-1:
+            return retornoTiposFish(texto)
+        if texto.find("BOAT") !=-1 or texto.find("SAIL")!=-1 or texto.find("ROWING")!=-1:
+            return "Sailing"
+        if texto.find("BATH"):
+            return "Bathing"
+        if texto.find("WALK") !=-1 or texto.find("WADIN") !=-1 or texto.find("WALK") !=-1 or texto.find("TREADING") !=-1:
+            return "Wading"
+
+def retornoScubaDiving(text):
+    if text.find("SCUB")!=-1:
+        return "Scuba diving"
+    else:
+        return "Diving"
+
+def retornoTiposSurf(text):
+    if text.find("BODY")!=-1:
+        return "Body surf"
+    if text.find("WIND")!=-1:
+        return "Windsurfing"
+    if text.find("SKI")!=-1:
+        return "Surf skiing"
+    if text.find("FIS")!=-1:
+        return "Surf fishing"
+    else:
+        return "Surfing"
+
+def retornoTiposFish(text):
+    if text.find("SPEAR")!=-1:
+        return "Spearfishing"
+    if text.find("SHARK")!=-1:
+        return "Shark fishing"
+    else:
+        return "Fishing"    
+
+def callChekcUrl(pdSerie):
+    return pdSerie.apply(checkUrlValid)
+
+
+def checkUrlValid(text):
+    """analiza si un string contiene una url válida (404 no valida) y la devuelve si lo es, si no devuelve UNKNOWN"""
+    r= requests.Response()
+    try:
+        
+        r = requests.get(text)
+        code = int(r.status_code)
+        del r
+        if code == 200:
+            return text
+        else:
+            return "UNKNOWN"
+    except:
+            return "UNKNOWN" 
 
