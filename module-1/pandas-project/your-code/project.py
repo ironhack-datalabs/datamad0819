16fef657a56bcc29f73a55c14635607c97732242
DavidR81 <davidrecuero@gmail.com>
2019-09-01 19:47:35 +0200

    seguimos avanzando

--- module-1/pandas-project/your-code/project.py
+++
@@ -1,6 +0,0 @@
-def type_of_gender(sex):  #funcion para dar una definicion a los NaN
-    if sex == 'M' or sex == 'F':
-        return sex
-    else:
-        return 'not defined'
-data['Sex'] = data['Sex'].apply(type_of_gender)º
