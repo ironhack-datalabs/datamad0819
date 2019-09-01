0cb94431758cbe593d3206b76e15b7823e2e2827
Jesus Rodríguez Álvarez <jralvarez1986@gmail.com>
2019-09-01 20:27:11 +0200

    new functions working well

--- module-1/pandas-project/your-code/modules/functions.py
+++ module-1/pandas-project/your-code/modules/functions.py
@@ -2,6 +2,8 @@ def get_poor_rows(df,nulls = 2):
     #returns indexes of rows with more than 2 Nulls in it.
     return [index for index, fila in df.iterrows() if fila.isnull().sum() > nulls]
 
-def drop_by_index(df, *indexes):
+
+
+def drop_by_index(df, indexes):
     #Drop rows by a given index or list of indexes.
     return df.drop(df.iloc[indexes].index)
