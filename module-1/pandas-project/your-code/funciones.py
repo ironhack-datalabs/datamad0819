417420d767d1d93540e6a6a0875f02940655b164
Your Name <jsl.carranza@gmail.com>
2019-09-02 10:45:23 +0200

    Terminado, faltan funciones

--- module-1/pandas-project/your-code/funciones.py
+++ module-1/pandas-project/your-code/funciones.py
@@ -9,14 +9,13 @@ def clean_year(año):
 
 
 def clean_month(month):
-    for i in month:
-        if len(i) > 3:
-           return month.replace(i,i[:3], inplace=True)
-        if i == "Jut":
-           return month.replace(i,'Jun', inplace=True)
+        if len(month) > 3:
+           return month.replace(month,month[:3])
+        elif month == "Jut":
+           return month.replace(month,'Jun')
         
 def cha_col_pos(col,pos,df):
-    
+
     lista_col = []
     for i in df.columns:
         lista_col.append(i)
@@ -27,8 +26,34 @@ def cha_col_pos(col,pos,df):
     return df[lista_col]
 
 def fatal_cl(Fatal):
-    for i in Fatal:
         if Fatal == 'N' or Fatal == 'Y':
             return Fatal
         else:
             return np.nan
+
+
+def dist_month(fecha):
+   
+    
+    mes_fila = ''.join(re.findall('\-[A-Za-z]{3}\-',fecha))
+
+    mes_fila = re.sub('\-','', mes_fila).capitalize()
+
+    if mes_fila == '':
+        mes_fila = np.nan
+
+
+    return mes_fila
+
+
+def dist_act(acty):
+        
+    act_fila = ''.join(re.findall(('Surfing|surfing|Surf|surf|Board|board'),acty))
+        
+    act_fila = re.sub('\-','', act_fila).capitalize()
+            
+            
+    if act_fila == '':
+        act_fila = np.nan
+
+    return act_fila
