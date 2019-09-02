f32edc8b4a66ea4ad5c37e6785964e7e157f5d82
hermenegildo martinez-alcala <hermenegildomartinez-alcala@MacBook-Pro-de-hermenegildo.local>
2019-09-02 12:33:35 +0200

    Minianalisis

--- module-1/pandas-project/your-code/funciones.py
+++ module-1/pandas-project/your-code/funciones.py
@@ -1,45 +1,51 @@
-def calendario(date):
-    mes = []
-    for e in df[Date]:
-        if e.find("Jan") != -1:
-            mes.append(1)
-        elif e.find("Feb") != -1:
-            mes.append(2)
-        elif e.find("Mar") != -1:
-            mes.append(3)
-        elif e.find("Apr") != -1:
-            mes.append(4)
-        elif e.find("May") != -1:
-            mes.append(5)
-        elif e.find("Jun") != -1:
-            mes.append(6)
-        elif e.find("Jul") != -1:
-            mes.append(7)
-        elif e.find("Aug") != -1:
-            mes.append(8)
-        elif e.find("Sep") != -1:
-            mes.append(9)
-        elif e.find("Oct") != -1:
-            mes.append(10)
-        elif e.find("Nov") != -1:
-            mes.append(11)
-        elif e.find("Dic") != -1:
-            mes.append(12)
-        else:
-            pass
-    return mes
+def calendario(e):
+#Funcion que recibe valores string y los pasa a mes de años.
+    if e.find("Jan") != -1:
+        return int(1)
+    elif e.find("Feb") != -1:
+        return int(2)
+    elif e.find("Mar") != -1:
+        return int(3)
+    elif e.find("Apr") != -1:
+        return int(4)
+    elif e.find("May") != -1:
+        return int(5)
+    elif e.find("Jun") != -1:
+        return int(6)
+    elif e.find("Jul") != -1:
+        return int(7)
+    elif e.find("Aug") != -1:
+        return int(8)
+    elif e.find("Sep") != -1:
+        return int(9)
+    elif e.find("Oct") != -1:
+        return int(10)
+    elif e.find("Nov") != -1:
+        return int(11)
+    elif e.find("Dic") != -1:
+        return int(12)
+    
 
 
-def estaciones(date):
-    estacion=[]
-    for i in df["Date"]:
-        if i==1 or i==2 or i==12:
-            estacion.append("invierno")
-        elif i==3 or i==4 or i==5:
-            estacion.append("primavera")
-        elif i==6 or i==7 or i==8:
-            estacion.append("verano")
-        else:
-            estacion.append("otoño")
-    return estacion
+def estaciones(i):
+#Funcion que recibe meses y te devuelve las estaciones.
+    if i==1 or i==2 or i==12:
+        return ("invierno")
+    elif i==3 or i==4 or i==5:
+        return("primavera")
+    elif i==6 or i==7 or i==8:
+        return("verano")
+    else:
+        return("otoño")
+
+def limpiar(df):
+#Funcion que remplaza los nombres de las columnas, eliminando espacios, valores en extremos.
+   limpiar = []
+   for col in df.columns:
+       col = col.strip().lower()
+       col = col.replace('.',' ')
+       limpiar.append(col)
+   df.columns = limpiar
+   return df.columns
+
 
