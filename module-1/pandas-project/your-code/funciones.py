b50b410b3dcdebc1156b5aa425745dc34766212e
Angel Conesa <anjel_ccb@hotmail.com>
2019-09-02 11:37:30 +0200

    cambios

--- module-1/pandas-project/your-code/funciones.py
+++ module-1/pandas-project/your-code/funciones.py
@@ -1,7 +1,7 @@
 def num_repetidas (columna1, columna2):
-    lista = 0
+    repetidos = 0
     for x in columna1:
         for y in columna2:
             if x == y:
-                lista += 1
-    return lista
+                repetidos += 1
+    return repetidos
