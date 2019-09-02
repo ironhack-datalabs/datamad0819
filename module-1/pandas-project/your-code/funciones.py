800dde64e2090dbff8df7d3f146ea43b37d99915
Maurizio Martin <macl34@msn.com>
2019-09-02 13:19:25 +0200

    Start new main to repeat the project

--- module-1/pandas-project/your-code/funciones.py
+++ module-1/pandas-project/your-code/funciones.py
@@ -1,12 +1,21 @@
 import re
 
-def fixYear(year):
+def fixAge(text):
+    text = str(text)
+    if text == "Teen":
+        return 16
+    elif text == '20s':
+        return 20
+    else:
+        return text
+
+def fixYear2(year):
     reg = re.findall('\\d{4}', year)
     if reg:
         return reg[0]
     return year
 
-def fixYear(date,year):
+def fixYear3(date,year):
     if year == 0:
         reg = re.findall('\\d{4}', year)
         if reg:
