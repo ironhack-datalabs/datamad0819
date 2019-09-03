de641a9cf287c6594fd1431d5584bd50c92fb708
Your Name <jsl.carranza@gmail.com>
2019-09-03 21:09:16 +0200

    Terminado

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -14,38 +14,42 @@ print(my_listComprehension)
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+square = [e**2 for e in range(1,21)]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+power_of_two = [2**e for e in range(1,51)]
+print(power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
+sqrt = [math.sqrt(e) for e in range(1,101)]
+print(sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+my_list = [-e for e in range(10,-1,-1)]
+print(my_list)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
+odds = [e for e in range(1,100) if e%2 == 0]
+print(odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven = [e for e in range(1,1000) if e/7 in [i for i in range(1,1000)] ]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -53,15 +57,18 @@ print(my_listComprehension)
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
+vocals= ['a','e','i','o','u']
+non_vowels = [e for e in vocals for e in teststring if e not in vocals]
+print(non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
+frase = 'The Quick Brown Fox Jumped Over The Lazy Dog'
 
-
+capital_letters = [e for e in frase if e.isupper()]
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
@@ -69,7 +76,8 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Remember to use list comprehensions and to print your results.
 
 
-
+consonants = [e for e in 'The quick brown fox jumped over the lazy dog' if e not in ['a','e','i','o','u']]
+print(consonants)
 
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
@@ -82,7 +90,11 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+import random
 
+random_lists = [[random.randint(1,101) for x in range(10)] for x in range(4)]
+
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
@@ -90,6 +102,8 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
+flatten_list = [i for e in list_of_lists for i in e]
+print(flatten_list)
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
@@ -99,11 +113,13 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
-
+flatten_list = [float(i) for e in list_of_lists for i in e]
+print(flatten_list)
 
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
+Este lo he hecho en el otro ejercicio, entiendo que es el mismo.
 
 for i in ['a','b','c']:
     print i**2
@@ -112,6 +128,8 @@ for i in ['a','b','c']:
 #15. Handle the exception thrown by the code below by using try and except blocks. 
 #Then use a finally block to print 'All Done.'
 # Check in provided resources the type of error you may use. 
+
+Este igual
 
 x = 5
 y = 0
@@ -122,7 +140,9 @@ z = x/y
 
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
-# Check in provided resources the type of error you may use. 
+# Check in provided resources the type of error you may use.
+
+Este tambien
 
 abc=[10,20,20]
 print(abc[3])
@@ -132,26 +152,37 @@ print(abc[3])
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
 
+def div(a,b):
+    try:
+        a/b
+        
+    except TypeError as e:
+        print(e)
+    except ZeroDivisionError as e:
+        print(e)
 
 
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-f = open('testfile','r')
-f.write('Test write this')
 
-
+try:
+    f = open('testfile', 'r')
+    f.write('Test write this')
+except IOError:
+    print("esto es un IOError!")
 
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
 
-fp = open('myfile.txt')
+try:
+    fp = open('myfile.txt')
     line = f.readline()
     i = int(s.strip())
-
-
+except (NameError, IOError):
+    print("exception")
 
 
 #20. The following function can only run on a Linux system. 
@@ -162,6 +193,11 @@ fp = open('myfile.txt')
 def linux_interaction():
     assert ('linux' in sys.platform), "Function can only run on Linux systems."
     print('Doing something.')
+    
+    try:
+        linux_interaction()
+    except:
+        print("exception")
 
 
 # Bonus Questions:
