34cc5c647cf37291112fa3032bcce419d20277f0
rociobetegon <rbetegonpedrejon@gmail.com>
2019-09-04 21:20:28 +0200

    challenge 1 and 2

--- module-1/lab-api-scavenger-game/your-code/challenge-1.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-1.py
@@ -1,1 +1,26 @@
-# enter your code below
+import requests #para hacer las llamadas apis
+import os #para moverte por el ordenador
+from dotenv import load_dotenv #para cargar fichero env
+load_dotenv() 
+git_token = os.getenv("git_token")
+
+def authRequest(url, params={}):
+    headers = {
+       "Authorization": "token {}".format(git_token)
+    }
+    response = requests.get(url,headers=headers, params=params)
+    return response.json()
+data = authRequest("https://api.github.com/users/ironhack-datalabs/repos")
+data
+forks_url = [ repos["forks_url"] for repos in data if repos["name"] == "datamad0819"]
+forks_url
+forks =  [authRequest(url) for url in forks_url]
+forks
+languages_url = [forkpepito["languages_url"] for lista in forks for forkpepito in lista ]
+languages_url
+languages_apis = [authRequest(url) for url in languages_url]
+languages_apis
+values = [(key)for diccionario in languages_apis for key in diccionario]
+values
+languages = list(set(values))
+print(languages)
--- module-1/lab-api-scavenger-game/your-code/challenge-2.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-2.py
@@ -1,1 +1,18 @@
-# enter your code below
+import requests #para hacer las llamadas apis
+import os #para moverte por el ordenador
+from dotenv import load_dotenv #para cargar fichero env
+load_dotenv() 
+git_token = os.getenv("git_token")
+
+def authRequest(url, params={}):
+    headers = {
+       "Authorization": "token {}".format(git_token)
+    }
+    response = requests.get(url,headers=headers, params=params)
+    return response.json()
+#Todos los commits
+data = authRequest("https://api.github.com/repos/ironhack-datalabs/datamad0819/commits")
+data
+#Los de la semana pasada
+data = authRequest("https://api.github.com/repos/ironhack-datalabs/datamad0819/commits?since=2019-08-26&until=2019-09-01")
+print(len(data))
