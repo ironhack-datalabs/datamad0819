54b4cda6a1cbcd96759f5ee61c6d08ceb91095fd
Jesus Rodríguez Álvarez <jralvarez1986@gmail.com>
2019-09-04 17:20:53 +0200

    part 1 of challenge 1 finished

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,18 +1,25 @@
 # enter your code below
 import os
-import dotenv
-import request
+from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 github_token = os.getenv("TOKEN")
-
+urlforks = 'https://api.github.com/repos/ironhack-datalabs/datamad0819/forks'
 
 def authRequest(url, params={}):
     headers = {
-       "Authorization": "token {}".format(github_token)
+        "Authorization": "token {}".format(github_token)
     }
     response = requests.get(url,headers=headers, params=params)
-    print(response.status_code)
-    #print(response.headers)
+    print('status:',response.status_code)
+    #print('headers',response.headers())
     return response.json()
+
+#lanzamos para conseguir los forks:
+
+forks = authRequest(urlforks)
+people_forking = [fork["owner"]["login"] for fork in forks]
+print(people_forking)
+
