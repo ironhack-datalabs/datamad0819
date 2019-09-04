24b5715d255de55ea5602253e781f4324625ed27
Dededavid <davidperez2121@gmail.com>
2019-09-04 19:07:03 +0200

    primeros cambios

---
+++ module-1/lab-api-scavenger-game/your-code/challenge-1.py
@@ -0,0 +1,30 @@
+import requests
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+key = 'GITHUB_TOKEN'
+github_token = os.getenv(key) 
+
+def authRequest(url, params={}):
+    headers = {
+       "Authorization": "token {}".format(github_token)
+    }
+    response = requests.get(url,headers=headers, params=params)
+    print(response.status_code)
+    return response.json()
+
+data = authRequest("https://api.github.com/repos/ironhack-datalabs/datamad0819/forks")
+datamad0819_list = list(dict.fromkeys([h["language"] for h in data]))
+print(datamad0819_list)
+
+#
+# parte 2
+data = authRequest("https://api.github.com/repos/ironhack-datalabs/datamad0819/commits")
+datamad0819_list = ([h["commit"] for h in data])
+x = len(datamad0819_list)
+print(x)
+    
+
+
+
