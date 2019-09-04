c4c2139c795ce76e947b199937bf6b5317ab49ef
Jesus Rodríguez Álvarez <jralvarez1986@gmail.com>
2019-09-04 23:19:17 +0200

    ejercicio 3 en pruebas

--- module-1/lab-api-scavenger-game/your-code/challenge-3.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-3.py
@@ -5,7 +5,7 @@ import requests
 load_dotenv()
 
 github_token = os.getenv("TOKEN")
-url = 'https://api.github.com/repos/ironhack-datalabs/scavenger/content/*.scavengerhunt'
+url = 'https://api.github.com/repos/ironhack-datalabs/scavenger/contents/'
 
 def authRequest(url, params={}):
     headers = {
@@ -13,9 +13,39 @@ def authRequest(url, params={}):
     }
     response = requests.get(url,headers=headers, params=params)
     print('status:',response.status_code)
-    #print('headers',response.headers())
+    
     return response.json()
 
-content = authRequest(url)
+folder = authRequest(url)
 
-print(content)
+foldername = [fol['name'] for fol in folder]
+
+
+def filesnames(lst):
+    scaven = []
+    for element in lst:
+        archivo = authRequest(url+element)
+        try:
+            for objeto in archivo:
+                scaven.append(objeto['path'])
+        except:
+            pass
+
+    retval =[path for path in scaven if path.endswith('scavengerhunt')]    
+    return retval
+
+def filecontent(lst):
+    words =[]
+    for path in lst:
+        archivo = authRequest(url+path)
+        words.append(archivo['content'])
+
+    return words
+
+
+filename = filesnames(foldername)
+palabras = filecontent(filename)
+
+print(foldername)
+print(filename)
+print(palabras)
