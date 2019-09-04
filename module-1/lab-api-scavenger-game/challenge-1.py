8f1444ea6003dfadfcb374152e0e3fbe844dc0e4
Jesus Rodríguez Álvarez <jralvarez1986@gmail.com>
2019-09-04 16:21:32 +0200

    structure created

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,18 @@
 # enter your code below
+import os
+import dotenv
+import request
+
+load_dotenv()
+
+github_token = os.getenv("TOKEN")
+
+
+def authRequest(url, params={}):
+    headers = {
+       "Authorization": "token {}".format(github_token)
+    }
+    response = requests.get(url,headers=headers, params=params)
+    print(response.status_code)
+    #print(response.headers)
+    return response.json()
