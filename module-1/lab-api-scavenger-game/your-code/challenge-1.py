86081c0e6838aaffb708b530345b182a507f01b2
Nacho-laiseca <ilaiseca@hotmail.com>
2019-09-04 17:40:40 +0200

    lab-api-scavenger-game: Challenge 1 done

--- module-1/lab-api-scavenger-game/your-code/challenge-1.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-1.py
@@ -1,2 +1,35 @@
 # enter your code below
 
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import base64
+
+git_token = os.getenv("git_token")
+
+
+def authRequest(url, params={}):
+    headers = {
+       "Authorization": "token {}".format(git_token)
+    }
+    response = requests.get(url,headers=headers, params=params)
+
+    return response.json()
+
+
+data = authRequest("https://api.github.com/users/ironhack-datalabs/repos") 
+
+forks_urls = [repo["forks_url"] for repo in data if repo["name"]=="datamad0819"]
+
+forks =[authRequest(ur) for ur in forks_urls]
+
+languages_url =[fork["languages_url"] for lista in forks for fork in lista]
+
+languages_forks = [authRequest(ur) for ur in languages_url]
+
+languages_list = [d for dt in languages_forks for d in dt]
+
+languages = list(set(languages_list))
+
+print(languages)
